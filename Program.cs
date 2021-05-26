6228e6f204ce2480e30191c70f9a28ba26e7aae6
Максим Ахматов <maksimahmatov2003@yandex.ru>
2021-05-26 12:29:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str;
+            Console.Write("Введите строку: ");
+            str = Console.ReadLine();
+            str = str.ToUpper();
+            Console.WriteLine("Полученная строка " + str);
+            Console.ReadKey();
+        }
+    }
+}
